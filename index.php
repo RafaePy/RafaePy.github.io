4d59fa838699a8924ebd016b73f23e2aa6abce0d
Rafae Dhopawakar <87798028+RafaePy@users.noreply.github.com>
2024-03-19 22:13:55 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+echo "hello world<br/>";
+$con = new mysqli("sql6.freemysqlhosting.net", "sql6692394", "QeYLmJgluQ", "sql6692394");
+if (!$con) {
+	echo "<h1>Connection failed</h1>";
+} else {
+echo "success<br/>";
+$q = "SELECT * FROM university";
+
+$res = $con->query($q);
+
+echo $res->num_rows;
+$data = $res->fetch_assoc();
+echo $data;
+echo "<h1>Name: " . $data['name'] . "<br/>" .
+"Location: " . $data['location'] . "<br/>" .
+"ID: " . $data['id'] . "<br/>" .
+"No. of courses: " . $data['courses'] . "</h1>";
+}
+?>
